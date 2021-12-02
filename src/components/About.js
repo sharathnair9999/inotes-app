47ae0a4b11a9cb7f081996407da4b2eca1c64966
Sharath <sharathnair9999@gmail.com>
2021-12-02 15:07:56 +0530

    updated about page

--- src/components/About.js
+++ src/components/About.js
@@ -4,8 +4,32 @@ import React from 'react'
 export const About = () => {
   // const a = useContext(noteContext)
   return (
-    <div>
-      This is About
+    <div className="accordion" id="accordionPanelsStayOpenExample">
+  <div className="accordion-item">
+    <h2 className="accordion-header" id="panelsStayOpen-headingOne">
+      <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+        Why iNotes?
+      </button>
+    </h2>
+    <div id="panelsStayOpen-collapseOne" className="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
+      <div className="accordion-body">
+         A secured note making app on cloud with email authentication. You will love using this app.
+      </div>
     </div>
+  </div>
+  <div className="accordion-item">
+    <h2 className="accordion-header" id="panelsStayOpen-headingTwo">
+      <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
+        What all can I do here?
+      </button>
+    </h2>
+    <div id="panelsStayOpen-collapseTwo" className="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingTwo">
+      <div className="accordion-body">
+        You can login if are already signed up. Else you can create your profile under 1 minute. You can create a new note, update it and delete it as per your needs
+        </div>
+    </div>
+  </div>
+ 
+</div>
   )
 }
